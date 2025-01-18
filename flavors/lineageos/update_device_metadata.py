59deeed3fe70e05afa5afae41f1cf388349423d5
Atemu <git@atemu.net>
2025-01-18 19:36:56 +0100

    lineageos: drop workarounds from metadata updater script
    We don't support these devices and will hopefully soon no longer require these
    hacks.

--- flavors/lineageos/update_device_metadata.py
+++ flavors/lineageos/update_device_metadata.py
@@ -47,33 +47,8 @@ def fetch_metadata(
         if data['model'] not in metadata:
             continue
 
-        workaround_map = {
-            # shamu needs a workaround as well
-            'shamu' : 'moto',
-            # Workaround google device names source tree inconsistency
-            'flox' : 'asus',
-            # wade is Google but uses askey vendor dirs? Dynalink is definitely wrong though.
-            'wade' : 'askey',
-            'deadpool' : 'askey',
-            # 10.or is apparently a vendor name. Why TF do you have to put dots in your name.
-            # TODO check whether we can exclude this case by always fetching from vendor_device for LOS-20 devices
-            'G' : '10or'
-        }
         device = data['model']
-        vendor = workaround_map[device] if device in workaround_map else data['oem'].lower()
-
-        # Workaround name inconsistency with LG
-        if vendor == 'lg':
-            vendor = 'lge'
-        # Look how cool my name is mom, parenthesis!
-        if vendor == 'f(x)tec':
-            vendor = 'fxtec'
-        # Urgh
-        if vendor == '10.or':
-            vendor = '10or'
-        # Really?
-        if vendor == 'banana pi':
-            vendor = 'bananapi'
+        vendor = data['oem'].lower()
 
         metadata[data['model']].update({
             'vendor': vendor,
