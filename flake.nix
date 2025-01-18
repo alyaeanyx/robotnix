ad4d64cd78efb5e631446c3370246333f8385c15
Atemu <git@atemu.net>
2025-01-18 20:19:43 +0100

    flake: update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Build Android (AOSP) using Nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     # FIXME Newer nixpkgs' git breaks our gitRepo hack because of wonky permissions
     nixpkgs-gitRepo.url = "github:NixOS/nixpkgs/5710852ba686cc1fd0d3b8e22b3117d43ba374c2";
